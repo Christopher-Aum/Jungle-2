4ea4bd751ccd05d7bb91954198869aba64015e5b
ChristopherAumonte <christopheraumonte@gmail.com>
2024-03-08 15:57:58 -0500

    Testing

--- react-vite/src/components/ItemPage/ItemPage.jsx
+++ react-vite/src/components/ItemPage/ItemPage.jsx
@@ -112,6 +112,7 @@ const ItemPage = () => {
                 {}
                     <span key={comment.id}>
                         <p>
+                            {console.log(comment.user_id.username)}
                             <h2>{comment.user_id.username}</h2> {postedAtDate(comment.updated_at)}
                         </p>
                         <p>{comment.body}</p>
